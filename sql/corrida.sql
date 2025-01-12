d317de2203a1f81289af98692eb72c7a8f58fb59
Julio35cesar <julioomelet@gmail.com>
2025-01-12 18:31:49 -0300

    Add files via upload

---
+++ sql/corrida.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE corrida_gamess;
+
+USE corrida_gamess;
+
+CREATE TABLE apostas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome_jogador VARCHAR(100) NOT NULL,
+    aposta_carro INT NOT NULL,
+    data_aposta TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE resultados (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    vencedor INT NOT NULL,
+    data_corrida TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
